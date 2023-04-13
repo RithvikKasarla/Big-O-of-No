3821ee8dc1ffe18349af524d3af9fc3edea14290
RithvikKasarla <rithvikrk719@gmail.com>
2023-04-13 18:26:51 -0400

    updates

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,35 +1,27 @@
-import App from './app';
-import bodyParser from 'body-parser';
-
-
+import App from "./app";
+import bodyParser from "body-parser";
 
 //auth controller
-import AuthController from './controllers/auth.controller';
+import AuthController from "./controllers/auth.controller";
 //class controller
-import ClassController from './controllers/class.controller';
+import ClassController from "./controllers/class.controller";
 //comment controller
-import CommentController from './controllers/comment.controller';
+import CommentController from "./controllers/comment.controller";
 //file controller
-import FileController from './controllers/file.controller';
+import FileController from "./controllers/file.controller";
 //user controller
-import UserController from './controllers/user.controller';
-
-
+import UserController from "./controllers/user.controller";
 
 const app = new App({
-    port: 3000,
-    controllers: [
-        new AuthController(),
-        new ClassController(),
-        new CommentController(),
-        new FileController(),
-        new UserController(),
-
-    ],
-    middlewares: [
-        bodyParser.json(),
-        bodyParser.urlencoded({ extended: true }),
-    ],
+  port: 3001,
+  controllers: [
+    new AuthController(),
+    new ClassController(),
+    new CommentController(),
+    new FileController(),
+    new UserController(),
+  ],
+  middlewares: [bodyParser.json(), bodyParser.urlencoded({ extended: true })],
 });
 
 app.listen();
