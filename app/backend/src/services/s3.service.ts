6f0796af57e46684f456c020203f67311a18ff16
trm109 <trm109@case.edu>
2023-04-18 18:18:56 -0400

    Dynamically checks for file mimetype

--- app/backend/src/services/s3.service.ts
+++ app/backend/src/services/s3.service.ts
@@ -24,7 +24,90 @@ class S3Service {
         let name = splitFilename[0];
         let extension = splitFilename[1];
         let iteration = 0;
-    
+        //split the filename by '.' and get the last element
+        const file_extension = filename.split('.').pop();
+        console.log(`File extension: ${file_extension}`);
+        if(!file_extension){
+            throw new Error(`Missing file extension for file ${filename}, ${file_extension}`);
+        }
+        let mime_type = '';
+        switch (file_extension) {
+            case 'png':
+                mime_type = 'image/png';
+                break;
+            case 'jpg':
+                mime_type = 'image/jpg';
+                break;
+            case 'jpeg':
+                mime_type = 'image/jpeg';
+                break;
+            case 'gif':
+                mime_type = 'image/gif';
+                break;
+            case 'mp4':
+                mime_type = 'video/mp4';
+                break;
+            case 'mp3':
+                mime_type = 'audio/mp3';
+                break;
+            case 'wav':
+                mime_type = 'audio/wav';
+                break;
+            case 'pdf':
+                mime_type = 'application/pdf';
+                break;
+            case 'doc':
+                mime_type = 'application/msword';
+                break;
+            case 'docx':
+                mime_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+                break;
+            case 'ppt':
+                mime_type = 'application/vnd.ms-powerpoint';
+                break;
+            case 'pptx':
+                mime_type = 'application/vnd.openxmlformats-officedocument.presentationml.presentation';
+                break;
+            case 'xls':
+                mime_type = 'application/vnd.ms-excel';
+                break;
+            case 'xlsx':
+                mime_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+                break;
+            case 'txt':
+                mime_type = 'text/plain';
+                break;
+            case 'html':
+                mime_type = 'text/html';
+                break;
+            case 'css':
+                mime_type = 'text/css';
+                break;
+            case 'js':
+                mime_type = 'text/javascript';
+                break;
+            case 'json':
+                mime_type = 'application/json';
+                break;
+            case 'xml':
+                mime_type = 'application/xml';
+                break;
+            case 'zip':
+                mime_type = 'application/zip';
+                break;
+            case 'rar':
+                mime_type = 'application/x-rar-compressed';
+                break;
+            case '7z':
+                mime_type = 'application/x-7z-compressed';
+                break;
+            case 'svg':
+                mime_type = 'image/svg+xml';
+                break;
+            default:
+                mime_type = 'application/octet-stream';
+                break;
+        }
         const findValidName = () => {
             return new Promise<string>(async resolve => {
                 const s3 = new S3(this.config);
@@ -59,7 +142,7 @@ class S3Service {
             Key: `cdn/${username}/${name}(${iteration}).${extension}`,
             Body: createReadStream(local_file_path), // add verification check.
             ACL: 'public-read',
-            ContentType: 'image/png', //make this dynamic
+            ContentType: mime_type, //make this dynamic
             ContentDisposition: 'inline'
         }
         try {
