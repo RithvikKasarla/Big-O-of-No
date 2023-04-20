7d46e6555d107b045915f35495f54ed5c9261f16
trm109 <trm109@case.edu>
2023-04-20 17:13:13 -0400

    added getComments

--- app/backend/src/controllers/comment.controller.ts
+++ app/backend/src/controllers/comment.controller.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response} from 'express';
 import AuthMiddleware from '../middleware/auth.middleware';
-import { File, Comment } from '@prisma/client';
+import { User, Comment } from '@prisma/client';
+import { body,param, query, validationResult } from 'express-validator';
 
 /*
 model Comment {
@@ -15,9 +16,12 @@ model Comment {
   @@index([fileId], map: "Comment_fileId_fkey")
 }
 */
+import UserService from '../services/user.service';
+import ClassService from '../services/class.service';
+import CommentService from '../services/comment.service';
 //These resources are only accessible via Cognito sign in.
 class CommentController {
-    public path = '/comment';
+    public path = '';
     public router = express.Router();
     private authMiddleware: AuthMiddleware;
 
@@ -30,16 +34,55 @@ class CommentController {
     public initializeTokenRoutes() {
         const tokenMiddleware = this.authMiddleware.verifyToken;
         //this.router.use(this.authMiddleware.verifyToken); //All functions after this require a token.
+        // Put a comment
+        // PUT URL/class/:classId/file/:fileId/
+        this.router.put(`/file/:fileId`, tokenMiddleware, this.createComment);
+        // Get Comments
+        // POST URL/class/:classId/file/:fileId/
+        this.router.post(`/file/:fileId`, tokenMiddleware, this.getComments);
+        // Delete a comment
+        // DELETE URL/class/:classId/file/:fileId/comment/:commentId
+        this.router.delete(`/file/:fileId/comment/:commentId`, tokenMiddleware, this.deleteComment);
+        
     }
     public initializeAdminRoutes() {
         const adminMiddleware = this.authMiddleware.verifyAdmin;
         //this.router.use(this.authMiddleware.verifyAdmin) //All functions after this require admin.
+        // Get all comments
+        // POST URL/comment/all
+        //Supported Queries: ?userId, ?classId, ?fileId, ?commentId
+        this.router.post(`/comment/all`, adminMiddleware, this.getAllComments);
+        // Force delete a comment
+        // DELETE URL/comment/:commentId
+        this.router.delete(`/comment/:commentId`, adminMiddleware, this.forceDeleteComment);
     }
     //Given the query parameters, return a list of comments.
     //Token is required.
     //Scoped to class.
+    //Suported Query Parameters: ?userId, ?classId, ?commentId
     getComments = async (request: Request, response: Response) => {
+        const {fileId } = request.params;
+        const {userId, classId, commentId} = request.query;
+        const { token } = request.body;
+        //Get userid from User Service.
+        const userService = new UserService();
+        //const user:User = await userService.getUser({token: token});
         
+        let getCommentsParams = {
+            fileId: parseInt(fileId.toString()), //Required.
+            ...(userId) ? {userId: parseInt(userId.toString())} : {},
+            ...(classId) ? {classId: parseInt(classId.toString())}: {},
+            ...(commentId) ? {commentId: parseInt(commentId.toString())}: {},
+        }
+
+        try {
+            const commentService = new CommentService();
+            const comments: Comment[] = await commentService.getComments(getCommentsParams);
+            return response.status(200).send(comments);
+        } catch (error) {
+            return response.status(500).send(error.message);
+        }
+
     }
 
     //Get ALL comments.
@@ -49,7 +92,29 @@ class CommentController {
     //Create a new comment.
     //Token is required.
     //Scoped to class.
-    createComment = async (request: Request, response: Response) => {}
+    createComment = async (request: Request, response: Response) => {
+        return response.status(501).send("Not Implemented")
+        const {fileId } = request.params;
+        const {content} = request.body;
+        const { token } = request.body;
+        //Get userid from User Service.
+        const userService = new UserService();
+        const user:User = await userService.getUser({token: token});
+        
+        let createCommentParams = {
+            fileId: parseInt(fileId.toString()), //Required.
+            content: content, //Required.
+            authorId: user.id, //Required.
+        }
+
+        try {
+            const commentService = new CommentService();
+            const comment: Comment = await commentService.createComment(createCommentParams);
+            return response.status(200).send(comment);
+        } catch (error) {
+            return response.status(500).send(error.message);
+        }
+    }
 
     //edit a comment.
     //Token is required.
@@ -64,5 +129,35 @@ class CommentController {
     //force delete a comment.
     //Admin only.
     forceDeleteComment = async (request: Request, response: Response) => {}
+
+    //Verify Body
+    private async validateBody(type: string) {
+        switch(type) {
+            case 'createComment':
+                return [
+                    param('fileId').isInt({min: 1}),
+                ]
+            case 'getComments':
+                return [
+                    param('fileId').isInt({min: 1}),
+                    query('userId').isInt({min: 1}),
+                    query('classId').isInt({min: 1}),
+                    query('commentId').isInt({min: 1}),
+                ]
+            case 'deleteComment':
+                return [
+                    param('fileId').isInt({min: 1}),
+                    param('commentId').isInt({min: 1}),
+                ]
+            case 'getAllComments':
+                return [
+
+                ]
+            case 'forceDeleteComment':
+                return [
+                    param('commentId').isInt({min: 1}),
+                ]
+        }
+    }
 }
 export default CommentController;
--- app/backend/src/services/comment.service.ts
+++ app/backend/src/services/comment.service.ts
@@ -2,16 +2,53 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 //import AWS from 'aws-sdk'
 //prisma
-import { User, PrismaClient } from '@prisma/client'
+import { User,Comment, PrismaClient } from '@prisma/client'
 import { promises } from 'dns';
 const prisma = new PrismaClient()
+/*
+model Comment {
+  id       Int    @id @default(autoincrement())
+  content  String
+  authorId Int
+  fileId   Int
+  author   User   @relation("UserComments", fields: [authorId], references: [id])
+  file     File   @relation("FileComments", fields: [fileId], references: [id])
 
+  @@index([authorId], map: "Comment_authorId_fkey")
+  @@index([fileId], map: "Comment_fileId_fkey")
+}
+*/
 class CommentService {
     constructor () {
 
     }
-    async getComments(){}
-    async createComment(){}
+    async getComments(
+        {fileId, userId,classId, commentId}:{fileId?: number, userId?: number, classId?: number,  commentId?: number}
+    ):Promise<Comment[]>{
+        try {
+            //Get the comments matching the query.
+            const comments = await prisma.comment.findMany({
+                where: {
+                    ...((fileId == -1) ? {} : {fileId: fileId}),
+                    ...((userId == -1) ? {} : {authorId: userId}),
+                    ...((classId == -1) ? {} : {file: {classId: classId}}),
+                    ...((commentId == -1) ? {} : {id: commentId})
+                }
+            });
+            return comments;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async createComment(
+        {}:{}
+    ){
+        try {
+            
+        } catch (error) {
+            throw error;
+        }
+    }
     async deleteComment(){}
     
 }
