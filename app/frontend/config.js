a4ff66c5578f13c8d37b4cc19f2550918855215d
trm109 <trm109@case.edu>
2023-04-24 20:27:49 -0400

    switched back to using served  backend

--- app/frontend/config.js
+++ app/frontend/config.js
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl: "http://localhost:3001",
-  //http://share-note.northcentralus.azurecontainer.io:3001
+  apiUrl: "http://share-note.northcentralus.azurecontainer.io:3001",
+  
 };
 
 export default config;
